8201100e411cb660f73cca5222c761422ea6effe
adejar12 <adejarjunior18@gmail.com>
2022-04-24 16:47:56 -0300

    feat: criando botões com rotas utilizando o router

--- next-basico/src/pages/routes/index.jsx
+++ next-basico/src/pages/routes/index.jsx
@@ -1,20 +1,61 @@
 import Link from "next/link";
+import router from "next/router";
 
 function routes() {
+  function navegacaoSimples(url) {
+    router.push(url);
+  }
+
+  function navegacaoComParams(url, parametros) {
+    router.push({ pathname: url, query: { ...parametros } });
+  }
+
   return (
-    <div>
-      <h1>Rotas</h1>
-      <ul>
-        <Link href={"/routes/params?nome=Jose&sobrenome=Junior"}>
-          <li>Params</li>
-        </Link>
-        <Link href={"/routes/123/buscar"}>
-          <li>[id]/buscar</li>
-        </Link>
-        <Link href={"/routes/123/João"}>
-          <li>[id]/[nome]</li>
-        </Link>
-      </ul>
+    <div style={{ marginLeft: 70 }}>
+      <h1>Tipos de Rotas</h1>
+      <li style={{ fontWeight: 800, fontSize: 18 }}>Navegação Com Link</li>
+      <div>
+        <ul>
+          <Link href={"/routes/123/buscar"}>
+            <li>[id]/buscar</li>
+          </Link>
+          <Link href={"/routes/params?nome=Jose&sobrenome=Junior"}>
+            <li>Params</li>
+          </Link>
+          <Link href={"/routes/123/João"}>
+            <li>[id]/[nome]</li>
+          </Link>
+        </ul>
+      </div>
+      <li style={{ fontWeight: 800, fontSize: 18, marginBottom: 25 }}>
+        Navegação com router
+      </li>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          height: 150,
+          justifyContent: "space-around",
+        }}
+      >
+        <button onClick={() => router.push("/routes/123/buscar")}>
+          [id]/buscar
+        </button>
+        <button
+          onClick={() =>
+            navegacaoComParams("/routes/params", {
+              nome: "Jose",
+              sobrenome: "Junior",
+            })
+          }
+        >
+          Params
+        </button>
+        <button onClick={() => navegacaoSimples("/routes/123/João")}>
+          [id]/[nome]
+        </button>
+      </div>
     </div>
   );
 }
