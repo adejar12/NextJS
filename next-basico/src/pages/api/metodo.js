3fef07f56b96a6da519beba81a53ab862bb91c27
adejar12 <adejarjunior18@gmail.com>
2022-04-24 17:05:07 -0300

    feat: adicionando exemplo de distinção de metodo na requisição

---
+++ next-basico/src/pages/api/metodo.js
@@ -0,0 +1,9 @@
+export default (req, res) => {
+  if (req.method === "GET") {
+    res.status(200).json({ name: "John Doe" });
+  } else {
+    res.status(200).json({
+      metodo: req.method,
+    });
+  }
+};
