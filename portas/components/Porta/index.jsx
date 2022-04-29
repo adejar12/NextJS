5c719c70ae1d1937657a097fc09876f3aed95a58
adejar12 <adejarjunior18@gmail.com>
2022-04-28 23:09:38 -0300

    feat: criando o component porta

---
+++ portas/components/Porta/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../../styles/Porta.module.css";
+
+// import { Container } from './styles';
+
+function Porta(props) {
+  return (
+    <div className={styles.porta}>
+      <div className={styles.numero}>3</div>
+    </div>
+  );
+}
+
+export default Porta;
