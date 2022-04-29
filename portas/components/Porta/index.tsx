708b64a97732fdb68d5d715e463c3f2c23cb1a06
adejar12 <adejarjunior18@gmail.com>
2022-04-29 10:38:32 -0300

    feat: change files to typescript

--- portas/components/Porta/index.tsx
+++ portas/components/Porta/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import styles from "../../styles/Porta.module.css";
+import PortaModel from "../../model/portaModel";
+
+interface PortaProps {
+  porta: PortaModel;
+}
 
 // import { Container } from './styles';
 
-function Porta({ selecionada }) {
-  const portaSelecionada = selecionada ? styles.selecionada : "";
+function Porta(props: PortaProps) {
+  const { porta } = props;
+
+  const portaSelecionada = porta.selecionada ? styles.selecionada : "";
 
   return (
     <div className={styles.area}>
       <div className={`${styles.estrutura}  ${portaSelecionada}`}>
         <div className={styles.porta}>
-          <div className={styles.numero}>3</div>
+          <div className={styles.numero}>{porta.numero}</div>
           <div className={styles.macaneta}></div>
         </div>
       </div>
