3aad88312db539b096bf57d02c8182c5fcc9934e
adejar12 <adejarjunior18@gmail.com>
2022-04-28 23:50:10 -0300

    styles: estilizando o component porta

--- portas/components/Porta/index.jsx
+++ portas/components/Porta/index.jsx
@@ -3,10 +3,18 @@ import styles from "../../styles/Porta.module.css";
 
 // import { Container } from './styles';
 
-function Porta(props) {
+function Porta({ selecionada }) {
+  const portaSelecionada = selecionada ? styles.selecionada : "";
+
   return (
-    <div className={styles.porta}>
-      <div className={styles.numero}>3</div>
+    <div className={styles.area}>
+      <div className={`${styles.estrutura}  ${portaSelecionada}`}>
+        <div className={styles.porta}>
+          <div className={styles.numero}>3</div>
+          <div className={styles.macaneta}></div>
+        </div>
+      </div>
+      <div className={styles.chao}></div>
     </div>
   );
 }
